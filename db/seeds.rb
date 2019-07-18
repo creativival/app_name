f6dc97405360bb84fa65629655d3f7a4c7d13456
creativial <creativival@gmail.com>
2019-07-18 10:31:21 +0900

    kaminar gem のテスト
    Userモデルの作成
    rails g kaminari:views bootstrap4 で専用のビューを作成（エラーは起きない）
    seed.rbより９９人のユーザーを作成
    rails g controller users index
    25人ずつ表示できることを確認　bootstrap4の導入も確認できた

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+99.times do |no|
+  if no < 10
+    User.create(name: "user0#{no}")
+  else
+    User.create(name: "user#{no}")
+  end
+end
